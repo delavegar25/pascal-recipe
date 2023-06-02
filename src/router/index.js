24e8a44b7eb027b79a7efaf6693b4546ecc8fdb4
Pascal <pascalokereke18@gmail.com>
2023-06-02 05:21:53 +0100

    first recipe

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView
+  },
+  {
+    path: '/recipe/:slug',
+    name: 'Recipe',
+    component: () => import('../views/Recipe.vue')
+  }
+  
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
